24ed54bc6313127e953cd3b14b967aefc07ae6f0
r4luthra <r4luthra@uwaterloo.ca>
2022-11-03 15:44:53 -0400

    Completed GUI landing page

--- source/src/main/kotlin/Main.kt
+++ source/src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
 import javafx.application.Application
+import java.lang.System.exit
 
 fun main(args: Array<String>) {
     val program : Program = Program()
-    program.runCLI()
-    Application.launch(MainApp::class.java)
+    var command = ""
+    while(command != "1" || command != "2") {
+        println("Menu\n1. Console Application\n2. Graphical Application\n3. Quit")
+        command = readLine()!!
+        if (command == "1") {
+            program.runCLI()
+            break
+        } else if (command == "2") {
+            Application.launch(MainApp::class.java)
+            break
+        } else if (command == "3") {
+            exit(0)
+        } else {
+            println("Invalid command. Please Try Again.")
+        }
+    }
+
+
 }
