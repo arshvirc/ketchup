f9c2019d834f098677cad8c1d874d4f9361abd63
ad-world <67097683+ad-world@users.noreply.github.com>
2022-11-10 17:28:18 -0500

    Create ID for TodoItem based on maximum ID present in DB. This way all of them will be unique.

--- source/service/src/main/kotlin/ketchup/service/controllers/ItemController.kt
+++ source/service/src/main/kotlin/ketchup/service/controllers/ItemController.kt
@@ -7,14 +7,32 @@ import java.sql.SQLException
 class ItemController(connection: Connection) {
     private val conn: Connection = connection
 
+    fun getTodoCount(): Int {
+        return try {
+            if(conn != null) {
+                val query = conn!!.createStatement()
+                val getCountQuery = "SELECT MAX(item_id) FROM TodoItems";
+                val result = query.executeQuery(getCountQuery);
+                result.getInt(1)
+            } else {
+                -1
+            }
+        } catch (ex: SQLException) {
+            println(ex.message)
+            -1
+        }
+    }
+
     fun addItem(item: TodoItem): Boolean {
         try {
             if (conn != null) {
+                val newId = getTodoCount();
+                val itemId = if(newId != -1) newId + 1 else item.id
                 val query = conn!!.createStatement()
                 // Add the actual item
                 val addItemQueryString = "INSERT INTO TodoItems(item_id, title, description, timestamp, deadline, priority, list_id, completion) " +
                         "VALUES (" +
-                        "\"${item.id}\", " +
+                        "\"${itemId}\", " +
                         "\"${item.title}\", " +
                         "\"${item.description}\", " +
                         "\"${item.timestamp}\"," +
@@ -35,7 +53,7 @@ class ItemController(connection: Connection) {
                         query.execute(addTagQueryString)
                     }
                     // Add item-tag pairing
-                    val addPairQueryString = "INSERT INTO ItemTags(item_id, tag) VALUES (\"${item.id}\", \"${tag}\");"
+                    val addPairQueryString = "INSERT INTO ItemTags(item_id, tag) VALUES (\"${itemId}\", \"${tag}\");"
                     query.execute(addPairQueryString)
                 }
             }
