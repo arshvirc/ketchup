7bc8b20ee4b7f699f28da8f0f4db10cca8986277
Arshvir <a85chaud@uwaterloo.ca>
2022-11-16 14:50:24 -0500

    Finished editing and deleting functionality

--- source/app/src/main/kotlin/ketchup/app/Model.kt
+++ source/app/src/main/kotlin/ketchup/app/Model.kt
@@ -20,28 +20,30 @@ class Model() {
     // We have the Actual Information:
     lateinit var dbListOfAllItems: TodoList                                 // Contains all TodoItems
     lateinit var dbListOfGroups: MutableList<TodoList>                      // Contains the Grouped List
+    lateinit var dbListOfCompletedItems: TodoList                           // Contains all CompletedItems
     lateinit var listOfTags: MutableList<String>                            // Contains the Tags
     lateinit var listOfGroups: MutableList<String>                          // Contains the Groups
+
     // Other Information
     val listOfPriorities: List<String> = listOf<String>("0", "1", "2", "3")
     var num = 0;
 
-    constructor(list: ObservableList<Node>): this() {
+    constructor(list: ObservableList<Node>) : this() {
         this.uiListOfAllItems = list
         this.dbListOfAllItems = TodoList()
+        this.dbListOfCompletedItems = TodoList()
         this.listOfTags = mutableListOf("Academic", "Family", "Extra")
         this.listOfGroups = mutableListOf("List 1", "List 2", "List 3")
         //this.callApiToUpdateModel()
     }
 
     fun callApiToUpdateModel() {
-        dbListOfAllItems = TodoList(dbListOfAllItems)               // replace w API call
+        // dbListOfAllItems = TodoList(dbListOfAllItems)               // replace w API call
         // dbListOfGroups
-        this.uiListOfAllItems = TodoListConverter(dbListOfAllItems) //
+        // this.uiListOfAllItems = TodoListConverter(dbListOfAllItems) //
         // uiListOfGroups
-        listOfTags = mutableListOf("Academic", "Family", "Extra")
-        listOfGroups = mutableListOf("List 1", "List 2", "List 3")
-
+        // listOfTags = mutableListOf("Academic", "Family", "Extra")
+        // listOfGroups = mutableListOf("List 1", "List 2", "List 3")
     }
 
 
@@ -53,211 +55,239 @@ class Model() {
         this.uiListOfAllItems.add(itemUI)       //Update UI List
         println(uiListOfAllItems.lastIndex)
     }
-}
 
-private fun TodoListConverter(dbList: TodoList) : ObservableList<Node> {
-    val uiList = FXCollections.observableArrayList<Node>()
-    for ( dbItem in dbList.list ) {
-        var uiItem = TodoItemConverter(dbItem)
-        uiList.add(uiItem)
-    }
-    return uiList
-}
-
-private fun TodoItemConverter(dbItem: TodoItem) : TitledPane {
-    val fxmlLoader = FXMLLoader(App::class.java.getResource("fxml/itemUI.fxml"))
-    val uiItem = fxmlLoader.load<TitledPane>()
-    uiItem.id = dbItem.id.toString()
-    uiItem.graphic = createItemGraphicUIComponent(dbItem)
-    uiItem.content = createItemContentUIComponent(dbItem)
-    println(dbItem.title)
-    return uiItem
-}
-
-private fun createItemGraphicUIComponent(item: TodoItem) : HBox {
-    var uiComponent = HBox()
-    var dragButton = Button()
-    dragButton.alignment = Pos.CENTER
-    dragButton.maxHeight = 25.0
-    dragButton.maxHeight = 25.0
-    dragButton.minHeight = 25.0
-    dragButton.minWidth = 25.0
-    dragButton.isMnemonicParsing = false
-    dragButton.prefHeight = 25.0
-    dragButton.prefWidth = 25.0
-    var dragImageView = ImageView()
-    dragImageView.fitHeight = 15.0
-    dragImageView.fitWidth = 40.0
-    dragImageView.isPickOnBounds = true
-    dragImageView.isPreserveRatio = true
-    var dragImage = Image("images/drag_icon.png")
-    dragImageView.image = dragImage
-    dragButton.graphic =dragImageView
-
-    var title = TextField()
-    title.maxHeight = 30.0
-    title.maxWidth = 1.7976931348623157E308
-    title.minHeight = 30.0
-    title.minWidth = Double.NEGATIVE_INFINITY
-    title.prefHeight = 30.0
-    title.prefWidth = 102.0
-    title.text = item.title
-    title.textProperty().addListener { observable, oldValue, newValue ->
-        run {
-            println(observable.toString())
-            println("Title changed from $oldValue to $newValue")
+    private fun TodoListConverter(dbList: TodoList): ObservableList<Node> {
+        val uiList = FXCollections.observableArrayList<Node>()
+        for (dbItem in dbList.list) {
+            var uiItem = TodoItemConverter(dbItem)
+            uiList.add(uiItem)
         }
+        return uiList
     }
 
-    var buttonBar = ButtonBar()
-    buttonBar.buttonMinWidth = 30.0
-    buttonBar.maxHeight = Double.NEGATIVE_INFINITY
-    buttonBar.maxWidth = Double.NEGATIVE_INFINITY
-    buttonBar.minHeight = 25.0
-    buttonBar.minWidth = Double.NEGATIVE_INFINITY
-    buttonBar.prefHeight = 30.0
-    buttonBar.prefWidth = 200.0
+    private fun TodoItemConverter(dbItem: TodoItem): TitledPane {
+        val fxmlLoader = FXMLLoader(App::class.java.getResource("fxml/itemUI.fxml"))
+        val uiItem = fxmlLoader.load<TitledPane>()
+        uiItem.id = dbItem.id.toString()
+        uiItem.graphic = createItemGraphicUIComponent(dbItem)
+        uiItem.content = createItemContentUIComponent(dbItem)
+        println(dbItem.title)
+        return uiItem
+    }
 
-    var completeButton = Button()
-    completeButton.alignment = Pos.CENTER
-    completeButton.maxHeight = 25.0
-    completeButton.maxHeight = 25.0
-    completeButton.minHeight = 25.0
-    completeButton.minWidth = 25.0
-    completeButton.isMnemonicParsing = false
-    completeButton.prefHeight = 25.0
-    completeButton.prefWidth = 25.0
+    private fun createItemGraphicUIComponent(item: TodoItem): HBox {
+        var uiComponent = HBox()
+        var dragButton = Button()
+        dragButton.alignment = Pos.CENTER
+        dragButton.maxHeight = 25.0
+        dragButton.maxHeight = 25.0
+        dragButton.minHeight = 25.0
+        dragButton.minWidth = 25.0
+        dragButton.isMnemonicParsing = false
+        dragButton.prefHeight = 25.0
+        dragButton.prefWidth = 25.0
+        var dragImageView = ImageView()
+        dragImageView.fitHeight = 15.0
+        dragImageView.fitWidth = 40.0
+        dragImageView.isPickOnBounds = true
+        dragImageView.isPreserveRatio = true
+        var dragImage = Image("images/drag_icon.png")
+        dragImageView.image = dragImage
+        dragButton.graphic = dragImageView
 
-    var completeImageView = ImageView()
-    completeImageView.fitHeight = 15.0
-    completeImageView.fitWidth = 40.0
-    completeImageView.isPickOnBounds = true
-    completeImageView.isPreserveRatio = true
-    var completeImage = Image("images/progress.png")
-    completeImageView.image = completeImage
-    completeButton.graphic = completeImageView
-    completeButton.graphic.id = "0"
-
-    completeButton.setOnAction { obs ->
-        run {
-            println(obs.toString())
-            val source = obs.source as Button
-            if ( source.graphic.id.toInt() == 1) {
-                var completedImage = Image("images/progress.png")
-                completeImageView.image = completedImage
-                completeButton.graphic = completeImageView
-                completeButton.graphic.id = "0"
-                // UPDATE MODEL AND UPDATE DATABASE
-            } else {
-                println("COMPLETED TASK")
-                var completedImage = Image("images/completed.png")
-                completeImageView.image = completedImage
-                completeButton.graphic = completeImageView
-                completeButton.graphic.id = "1"
-                println("Parent Id:  + ${completeButton.parent.parent.parent.id}")
-                // UPDATE MODEL AND UPDATE DATABASE
+        var title = TextField()
+        title.maxHeight = 30.0
+        title.maxWidth = 1.7976931348623157E308
+        title.minHeight = 30.0
+        title.minWidth = Double.NEGATIVE_INFINITY
+        title.prefHeight = 30.0
+        title.prefWidth = 102.0
+        title.text = item.title
+        title.textProperty().addListener { observable, oldValue, newValue ->
+            run {
+                println(observable.toString())
+                println("Title changed from $oldValue to $newValue")
             }
         }
-    }
 
-    uiComponent.children.addAll(dragButton,title,completeButton)
+        var buttonBar = ButtonBar()
+        buttonBar.buttonMinWidth = 30.0
+        buttonBar.maxHeight = Double.NEGATIVE_INFINITY
+        buttonBar.maxWidth = Double.NEGATIVE_INFINITY
+        buttonBar.minHeight = 25.0
+        buttonBar.minWidth = Double.NEGATIVE_INFINITY
+        buttonBar.prefHeight = 30.0
+        buttonBar.prefWidth = 200.0
 
-    return uiComponent
-}
+        var completeButton = Button()
+        completeButton.alignment = Pos.CENTER
+        completeButton.maxHeight = 25.0
+        completeButton.maxHeight = 25.0
+        completeButton.minHeight = 25.0
+        completeButton.minWidth = 25.0
+        completeButton.isMnemonicParsing = false
+        completeButton.prefHeight = 25.0
+        completeButton.prefWidth = 25.0
 
-private fun createItemContentUIComponent(item: TodoItem) : VBox {
-    var vbox = VBox()
-    vbox.prefHeight = 200.0
-    vbox.prefWidth = 100.0
+        var completeImageView = ImageView()
+        completeImageView.fitHeight = 15.0
+        completeImageView.fitWidth = 40.0
+        completeImageView.isPickOnBounds = true
+        completeImageView.isPreserveRatio = true
+        var completeImage = Image("images/progress.png")
+        completeImageView.image = completeImage
+        completeButton.graphic = completeImageView
+        completeButton.graphic.id = "0"
 
-    var details = TextField()
-    details.prefHeight = 20.0
-    details.prefWidth = 331.0
-    if (item.description == "") {
-        details.promptText = "Enter Details"
-    } else {
-        details.text = item.description
-    }
-    details.textProperty().addListener { observable, oldValue, newValue ->
-        run {
-            println(observable.toString())
-            println("textfield3 changed from $oldValue to $newValue")
+        completeButton.setOnAction { obs ->
+            run {
+                println(obs.toString())
+                val source = obs.source as Button
+                if (source.graphic.id.toInt() == 1) {
+                    var completedImage = Image("images/progress.png")
+                    completeImageView.image = completedImage
+                    completeButton.graphic = completeImageView
+                    completeButton.graphic.id = "0"
+                    val itemId = completeButton.parent.parent.parent.id
+                    this.removeCompletedItem(itemId)
+                    // UPDATE MODEL AND UPDATE DATABASE
+                } else {
+                    println("COMPLETED TASK")
+                    var completedImage = Image("images/completed.png")
+                    completeImageView.image = completedImage
+                    completeButton.graphic = completeImageView
+                    completeButton.graphic.id = "1"
+                    println("Parent Id:  + ${completeButton.parent.parent.parent.id}")
+                    // UPDATE MODEL AND UPDATE DATABASE
+                    val itemId = completeButton.parent.parent.parent.id
+                    this.removeCompletedItem(itemId)
+                }
+            }
         }
+
+        uiComponent.children.addAll(dragButton, title, completeButton)
+
+        return uiComponent
     }
 
-    vbox.children.add(details)
+    private fun createItemContentUIComponent(item: TodoItem): VBox {
+        var vbox = VBox()
+        vbox.prefHeight = 200.0
+        vbox.prefWidth = 100.0
 
-    var tagsBox = HBox()
-    tagsBox.prefHeight = 100.0
-    tagsBox.prefWidth = 200.0
-
-    var tagName = Label()
-    tagName.prefHeight = 17.0
-    tagName.prefWidth = 70.0
-    tagName.text = "Tags: "
-    tagsBox.children.add(tagName)
-
-    var tagOptions = ComboBox<String>()
-    tagOptions.prefHeight = 26.0
-    tagOptions.prefWidth = 117.0
-    tagOptions.value = "Not Implemented Yet"
-    tagsBox.children.add(tagOptions)
-    // tagsBox end
-
-    // deadlineBox start
-    var deadlineBox = HBox()
-    deadlineBox.prefHeight = 100.0
-    deadlineBox.prefWidth = 200.0
-
-    var deadlineName = Label()
-    deadlineName.prefHeight = 17.0
-    deadlineName.prefWidth = 71.0
-    deadlineName.text = "Deadline: "
-    deadlineBox.children.add(deadlineName)
-
-    var deadlineOption = DatePicker()
-    deadlineOption.prefHeight = 26.0
-    deadlineOption.prefWidth = 120.0
-    // deadlineOption.value = item.deadline
-    deadlineBox.children.add(deadlineOption)
-    // deadlineBox end
-
-    // priorityBox start
-    var priorityBox = HBox()
-    priorityBox.prefHeight = 100.0
-    priorityBox.prefWidth = 200.0
-
-    var priorityName = Label()
-    priorityName.prefHeight = 17.0
-    priorityName.prefWidth = 71.0
-    priorityName.text = "Priority: "
-    priorityBox.children.add(priorityName)
-
-    var priorityOptions = ComboBox<String>()
-    priorityOptions.prefHeight = 26.0
-    priorityOptions.prefWidth = 120.0
-    //priorityOptions.items.addAll(listOfPriorities)
-    priorityOptions.value = item.priority.toString()
-    priorityBox.children.add(priorityOptions)
-    // deadlineBox end
-
-    var buttonBar = ButtonBar()
-    buttonBar.prefHeight = 40.0
-    buttonBar.prefWidth = 200.0
-
-    var button = Button()
-    button.text = "Trash"
-    buttonBar.buttons.add(button)
-    vbox.children.add(tagsBox)
-    vbox.children.add(deadlineBox)
-    vbox.children.add(priorityBox)
-    vbox.children.add(buttonBar)
-    button.setOnAction { obs ->
-        run {
-            val id = button.parent.parent.parent.parent.parent.id
-            println("$id ${button.parent.parent.parent.parent.parent}")
+        var details = TextField()
+        details.prefHeight = 20.0
+        details.prefWidth = 331.0
+        if (item.description == "") {
+            details.promptText = "Enter Details"
+        } else {
+            details.text = item.description
         }
+        details.textProperty().addListener { observable, oldValue, newValue ->
+            run {
+                println(observable.toString())
+                println("textfield3 changed from $oldValue to $newValue")
+            }
+        }
+
+        vbox.children.add(details)
+
+        var tagsBox = HBox()
+        tagsBox.prefHeight = 100.0
+        tagsBox.prefWidth = 200.0
+
+        var tagName = Label()
+        tagName.prefHeight = 17.0
+        tagName.prefWidth = 70.0
+        tagName.text = "Tags: "
+        tagsBox.children.add(tagName)
+
+        var tagOptions = ComboBox<String>()
+        tagOptions.prefHeight = 26.0
+        tagOptions.prefWidth = 117.0
+        tagOptions.value = "Not Implemented Yet"
+        tagsBox.children.add(tagOptions)
+        // tagsBox end
+
+        // deadlineBox start
+        var deadlineBox = HBox()
+        deadlineBox.prefHeight = 100.0
+        deadlineBox.prefWidth = 200.0
+
+        var deadlineName = Label()
+        deadlineName.prefHeight = 17.0
+        deadlineName.prefWidth = 71.0
+        deadlineName.text = "Deadline: "
+        deadlineBox.children.add(deadlineName)
+
+        var deadlineOption = DatePicker()
+        deadlineOption.prefHeight = 26.0
+        deadlineOption.prefWidth = 120.0
+        // deadlineOption.value = item.deadline
+        deadlineBox.children.add(deadlineOption)
+        // deadlineBox end
+
+        // priorityBox start
+        var priorityBox = HBox()
+        priorityBox.prefHeight = 100.0
+        priorityBox.prefWidth = 200.0
+
+        var priorityName = Label()
+        priorityName.prefHeight = 17.0
+        priorityName.prefWidth = 71.0
+        priorityName.text = "Priority: "
+        priorityBox.children.add(priorityName)
+
+        var priorityOptions = ComboBox<String>()
+        priorityOptions.prefHeight = 26.0
+        priorityOptions.prefWidth = 120.0
+        //priorityOptions.items.addAll(listOfPriorities)
+        priorityOptions.value = item.priority.toString()
+        priorityBox.children.add(priorityOptions)
+        // deadlineBox end
+
+        var buttonBar = ButtonBar()
+        buttonBar.prefHeight = 40.0
+        buttonBar.prefWidth = 200.0
+
+        var button = Button()
+        button.text = "Trash"
+        buttonBar.buttons.add(button)
+        vbox.children.add(tagsBox)
+        vbox.children.add(deadlineBox)
+        vbox.children.add(priorityBox)
+        vbox.children.add(buttonBar)
+        button.setOnAction { obs ->
+            run {
+                val id = button.parent.parent.parent.parent.parent.id
+                println("$id ${button.parent.parent.parent.parent.parent}")
+                this.removeDeletedItem(id)
+            }
+        }
+
+        return vbox
+    }
+    private fun removeCompletedItem( completedID: String) {
+        val tempList = FXCollections.observableArrayList<Node>()
+        for (item in uiListOfAllItems) {
+            if ( item.id == completedID ) {
+                tempList.add(item)
+            }
+        }
+        uiListOfAllItems.removeAll(tempList)
+        uiListOfAllItems.addAll(tempList)
+        print(uiListOfAllItems.lastIndex)
     }
 
-    return vbox
+    private fun removeDeletedItem( completedID: String) {
+        val tempList = FXCollections.observableArrayList<Node>()
+        for (item in uiListOfAllItems) {
+            if ( item.id == completedID ) {
+                tempList.add(item)
+                println()
+            }
+        }
+        uiListOfAllItems.removeAll(tempList)
+    }
+
 }
